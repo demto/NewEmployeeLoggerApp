0062fb3190af0ef4ba9973c92303022eeed50ba6
Tom <demto13@gmail.com>
2018-04-07 20:48:45 +0100

    NewEmployeeLogger added

---
+++ NewEmployeeLoggerApp/EmployeeLogger.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewEmployeeLoggerApp {
+    class EmployeeLogger {
+
+        private void LogNewEmployee(Employee employee){
+            string path = @"C:\MyStuff\AppLogs\NewEmployee.txt";
+            using (StreamWriter sr = new StreamWriter(path, true)){
+                string log = $"{employee.Name} has been created at {DateTime.Now} with ID of {employee.EmployeeId}";
+                sr.WriteLine(log);
+            }
+        }
+
+        public void NewEmployeeAdded(object sender, EventArgs e){
+            LogNewEmployee(e.Employee);
+        }
+    }
+}
